235fa69f99d3c3cd78c4e98bbfb11025df5e3e37
jacob-goodall <63982530+jacob-goodall@users.noreply.github.com>
2021-08-09 09:30:07 +1200

    DEF ing (Functions)

--- Names+Gui+amount-people.py
+++ Names+Gui+amount-people.py
@@ -1,14 +1,10 @@
 import turtle, time
-
-
-
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 filepath = ""
 def open_file():
-    global filepath
-    global sorted_new_list
+    global filepath, sorted_new_list, window
     filepath = askopenfilename(
         filetypes=[("Text Files", "*.txt"), ("CSV", "*.csv*")]
     )
@@ -17,9 +13,6 @@ def open_file():
     print(filepath)
     window.title(f"File Selector - {filepath}")
 
-    f = open(filepath,'r')
-    message = f.read()
-    print(message)
     with open(filepath) as a:
         lines = a.readlines()
     new_list = [s.replace("\n", "") for s in lines]
@@ -27,7 +20,6 @@ def open_file():
     sorted_new_list = sorted(new_list)
     print(sorted_new_list)
     
-    f.close()
     file = open(filepath,"r")
     Counter = 0
     
@@ -37,68 +29,73 @@ def open_file():
     for i in CoList:
         if i:
             Counter += 1
+    file.close()
 
     print("Number of People in List:",Counter)
     for elem in sorted_new_list:
           print(elem)
+    
+def tkint():
+  global window
+  window = tk.Tk()
+  window.title("File Selector")
+  
 
+  txt_edit = tk.Text(window)
+  fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+  btn_open = tk.Button(fr_buttons, text="Open Files", command=open_file)
+  btn_open.grid(row=0, column=0, sticky="ew", padx=0, pady=0)
+  fr_buttons.grid(row=0, column=0, sticky="ns")
+  
 
+  window.mainloop()
+  turtles()
 
-window = tk.Tk()
-window.title("File Selector")
+def turtles():
 
-txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(fr_buttons, text="Open Files", command=open_file)
-btn_open.grid(row=0, column=0, sticky="ew", padx=0, pady=0)
-fr_buttons.grid(row=0, column=0, sticky="ns")
+  WIDTH, HEIGHT = 800, 800
 
-window.mainloop()
+  screen = turtle.Screen()
+  screen.setup(WIDTH, HEIGHT)  # fudge factors due to window borders & title bar
+  screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
+  turtle.tracer(0)
+  flag = turtle.Turtle()
 
+  start = 100
+  flag.penup()
+  flag.goto(0,50)
+  for i in range(3):
+      for i in range (8):
+          flag.penup()
+          flag.forward(150)
+          flag.dot()
+          flag.penup()
+      
+      flag.goto(0, start)
+      start = start + 50
 
+  start1 = 100
+  order = 0 
+  name = turtle.Turtle()
+  name.penup()
+  name.goto(0,50)
 
-WIDTH, HEIGHT = 800, 800
+  l = [ValueError, TypeError]
 
-screen = turtle.Screen()
-screen.setup(WIDTH, HEIGHT)  # fudge factors due to window borders & title bar
-screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
-turtle.tracer(0)
-flag = turtle.Turtle()
+  for  x in range(3):
+      for x in range(8):
+        name.penup()
+        name.forward(150)
+        name.write(sorted_new_list[order])
+        order += 1 
+        if order >= 17:
+          break
+          time.sleep(100)
+          
+          
+      name.goto(0, start1)
+      start1 = start1 + 50     
 
-start = 100
-flag.penup()
-flag.goto(0,50)
-for i in range(3):
-    for i in range (8):
-        flag.penup()
-        flag.forward(150)
-        flag.dot()
-        flag.penup()
-        
+  turtle.done()
 
-    flag.goto(0, start)
-    start = start + 50
-
-start1 = 100
-order = 0 
-name = turtle.Turtle()
-name.penup()
-name.goto(0,50)
-
-l = [ValueError, TypeError]
-
-for  x in range(3):
-    for x in range(8):
-      name.penup()
-      name.forward(150)
-      name.write(sorted_new_list[order])
-      order += 1 
-      if order >= 17:
-        break
-        time.sleep(100)
-        
-    name.goto(0, start1)
-    start1 = start1 + 50     
-
-turtle.done()
-file.close()
+tkint()
