3e7aad8abc6fabdd25e523a14d0592b681aadd7f
jacob-goodall <63982530+jacob-goodall@users.noreply.github.com>
2021-08-12 23:16:10 +1200

    Final Sorting By War

--- scratch.py
+++ scratch.py
@@ -3,7 +3,7 @@ with open("Level3crosses.txt") as a:
     lines = a.readlines()
 new_list = [s.replace("\n", "") for s in lines]
 sorted_new_list = sorted(new_list)
-print(sorted_new_list)
+
 
 first = [i.split(' ')[0] for i in sorted_new_list] 
 second = [i.split(' ')[1] for i in sorted_new_list] 
@@ -12,29 +12,37 @@ third = [i.split(' ')[2] for i in sorted_new_list]
 
 test = list(chain.from_iterable(zip(first, second, third)))
 chunks = [test[x:x+3] for x in range(0, len(test), 3)]
-print(chunks[2][2])
+
 
 def Sort(chunks):
  
     chunks.sort(key = lambda x: x[2])
     return chunks
 
-print(Sort(chunks))
+Sort(chunks)
 order = 0
 boer = 0 
 ww1 = 0 
 ww2 = 0
 
 listing = list(map(' '.join, chunks))
-print(listing)
+
 
 for x in listing:
     if "BOER" in x:
         boer += 1      
-    if "WWI" in x:
+    if "WWI" and not "WWII" in x:
         ww1 += 1 
     if "WWII" in x:
         ww2 += 1
+ww1 = ww2 - ww1
+war_boer = listing[:boer]
+war_1 = listing[boer:boer + ww1]
+war_2 = listing[boer + ww1 :]
 
-war_boer = listing 
-        
+
+sorted_war_boer = sorted(war_boer)
+sorted_war_1 = sorted(war_1)
+sorted_war_2 = sorted(war_2)
+
+
